6ad84f985f6eb1ec35e42c8457e3ad2eb4c9eb53
Cristian Ciacu <d.c.ciacu@student.tudelft.nl>
2023-02-26 20:16:01 +0100

    colored nodes when adding an edge between two nodes

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 
-class Index extends React.Component<{}, {mousePosX: number, mousePosY: number, nodes: string}> {
+class Index extends React.Component<{}, {mousePosX: number, mousePosY: number, nodeId: number, nodes: {id: number, node: any} [], 
+selectedNode: string}> {
     constructor(props: {} | Readonly<{}>) {
         super(props)
         
         this.state = {
             mousePosX: 0,
             mousePosY: 0,
-            nodes: ''
+            nodes: [],
+            nodeId: 0,
+            selectedNode: ''
         }
     }
 
@@ -18,25 +21,74 @@ class Index extends React.Component<{}, {mousePosX: number, mousePosY: number, n
         })
     }
 
+    _connectTwoStates(id: number) {
+        let currButton = document.getElementById(id.toString())
+        if (this.state.selectedNode !== '') { // If we already selected a starting state
+            // TODO: Draw an edge from selectedNode to the current node.
+
+            console.log(this.state.selectedNode)
+            // Color the current node and wait one second.
+            // After that second revert the colors of both nodes
+            currButton?.classList.remove("unpressedButton")
+            currButton?.classList.add("pressedButton")
+            let selectedState = document.getElementById(this.state.selectedNode)
+
+            setTimeout(() => {
+                // Revert to the original colors of both nodes
+                selectedState?.classList.remove("pressedButton")
+                currButton?.classList.remove("pressedButton")
+                selectedState?.classList.add("unpressedButton")
+                currButton?.classList.add("unpressedButton")
+            }, 1000)
+
+            console.log("second")
+
+            // Now, we need to delete the old selected node
+            this.setState({
+                selectedNode: ''
+            })
+
+        } else {
+            currButton?.classList.remove("unpressedButton")
+            currButton?.classList.add("pressedButton")
+            this.setState({
+                selectedNode: id.toString()
+            })
+            console.log("first")
+        }
+    }
+
     _createNode(e: any) {
-        this.setState({
-            nodes: this.state.nodes.concat(`<div style='width: 50px; height: 50px; background-color: white; 
-                left: calc(${this.state.mousePosX}px - 25px); 
-                top: calc(${this.state.mousePosY}px - 25px); 
-                position: absolute;
-                z-index: -1'>
-            </div>`) 
-        })
-        return { __html: this.state.nodes }
+        let node = 
+        <button style={{width: '50px', height: '50px',
+                left: this.state.mousePosX - 25,
+                top: this.state.mousePosY - 25,
+                position: 'absolute', zIndex: -1}} 
+            id={this.state.nodeId.toString()} 
+            onClick={this._connectTwoStates.bind(this, this.state.nodeId)}
+            className={"unpressedButton"}>
+        </button>;
+
+        const newLocal = this;
+        newLocal.setState(prevState => ({
+            nodes: [...prevState.nodes, {id: prevState.nodeId, node: node}],
+            nodeId: prevState.nodeId + 1
+        }))
     }
 
     render() {
         const {mousePosX, mousePosY, nodes} = this.state;
 
+        const displayNodes = nodes.map((node) => 
+            <div key = {node.id}>
+                {node.node}
+            </div>
+        );
+
         return(
             <div>
                 <div className="background" onMouseMove={this._onMouseMove.bind(this)} onClick = {this._createNode.bind(this)}></div>
-                <div dangerouslySetInnerHTML={{__html: nodes}}></div>
+                {displayNodes}
             </div>
         )
     }
